5b19493db29a1f66e38f19721e996287601827a7
Kyler Davis <kyler.davis@maritzcx.com>
2019-06-23 15:56:06 -0600

    More DB stuff.

--- app/src/main/java/com/example/kylerdavisc196project/DbHandler.java
+++ app/src/main/java/com/example/kylerdavisc196project/DbHandler.java
@@ -32,20 +32,157 @@ public class DbHandler extends SQLiteOpenHelper {
                     + TERM_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
                     + TERM_NAME + " TEXT, "
                     + TERM_START_DATE + " DATETIME, "
-                    + TERM_END_DATE + " DATETIME" + ")";
+                    + TERM_END_DATE + " DATETIME)";
 
-    //Term Table Insert
-    public long insertTerm(Term term) {
-        SQLiteDatabase db = this.getWritableDatabase();
+    //Mentor Table
+    public static final String TABLE_MENTOR = "mentor";
+    public static final String MENTOR_ID = "mentorId";
+    public static final String MENTOR_NAME = "courseMentorName";
+    public static final String MENTOR_PHONE = "courseMentorPhone";
+    public static final String MENTOR_EMAIL = "courseMentorEmail";
 
-        ContentValues values = new ContentValues();
-        values.put(TERM_NAME, term.getName());
-        values.put(TERM_START_DATE, term.getStartDate());
-        values.put(TERM_END_DATE, term.getEndDate());
+    //Mentor Table Create
+    private static final String CREATE_MENTOR_TABLE =
+            "CREATE TABLE IF NOT EXISTS " + TABLE_MENTOR + " ("
+                    + MENTOR_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + MENTOR_NAME + " TEXT, "
+                    + MENTOR_PHONE + " TEXT, "
+                    + MENTOR_EMAIL + " TEXT)";
+    //Status Table
+    public static final String TABLE_STATUS = "status";
+    public static final String STATUS_ID = "statusId";
+    public static final String STATUS_NAME = "statusName";
 
-        long termId = db.insert(TABLE_TERM, null, values);
+    //Status Table Create
+    private  static final String CREATE_STATUS_TABLE =
+            "CREATE TABLE IF NOT EXISTS " + TABLE_STATUS + " ("
+                    + STATUS_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + STATUS_NAME + " TEXT)";
 
-        return termId;
+    //Course Table
+    public static final String TABLE_COURSE = "course";
+    public static final String COURSE_ID = "courseId";
+    public static final String COURSE_NAME = "courseName";
+    public static final String COURSE_DESCRIPTION = "courseDescription";
+    public static final String COURSE_START_DATE = "courseStartDate";
+    public static final String COURSE_END_DATE = "courseEndDate";
+    public static final String COURSE_MENTOR_ID = "courseMentorId";
+    public static final String COURSE_STATUS_ID = "courseStatus";
+    public static final String COURSE_TERM_ID = "courseTermId";
+
+    //Course Table Create
+    private static final String CREATE_COURSE_TABLE =
+            "CREATE TABLE IF NOT EXISTS " + TABLE_COURSE + " ("
+            + COURSE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + COURSE_NAME + " TEXT, "
+            + COURSE_DESCRIPTION + " TEXT, "
+            + COURSE_START_DATE + " DATE, "
+            + COURSE_END_DATE + " DATE, "
+            + COURSE_MENTOR_ID + " INTEGER, "
+            + COURSE_STATUS_ID + " INTEGER, "
+            + COURSE_TERM_ID + " INTEGER,"
+            + "FOREIGN KEY ("+COURSE_MENTOR_ID+") REFERENCES " + TABLE_MENTOR + "("+MENTOR_ID+") ON DELETE CASCADE,"
+            + "FOREIGN KEY ("+COURSE_STATUS_ID+") REFERENCES " + TABLE_STATUS + "("+STATUS_ID+") ON DELETE CASCADE,"
+            + "FOREIGN KEY ("+COURSE_TERM_ID+") REFERENCES " + TABLE_COURSE + "("+TERM_ID+") ON DELETE CASCADE)";
+    //Assessment Type Table
+    public static final String TABLE_ASSESSMENT_TYPE = "assessmentType";
+    public static final String ASSESSMENT_TYPE_ID = "assessmentTypeId";
+    public static final String ASSESSMENT_TYPE_NAME = "assessmentTypeName";
+
+    //Assessment Type Table Create
+    private static final String CREATE_ASSESSMENT_TYPE_TABLE =
+            "CREATE TABLE IF NOT EXISTS " + TABLE_ASSESSMENT_TYPE + " ("
+                    + ASSESSMENT_TYPE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + ASSESSMENT_TYPE_NAME + " TEXT)";
+
+    //Assessment Table
+    public static final String TABLE_ASSESSMENT = "assessment";
+    public static final String ASSESSMENT_ID = "assessmentId";
+    public static final String ASSESSMENT_NAME = "assessmentName";
+    public static final String ASSESSMENT_DUE_DATE = "assessmentDueDate";
+    public static final String ASSESSMENT_ASSESSMENT_TYPE_ID = "assessmentAssessmentTypeId";
+    public static final String ASSESSMENT_COURSE_ID = "assessmentCourseId";
+
+    //Assessment Table Create
+    private static final String CREATE_ASSESSMENT_TABLE =
+            "CREATE TABLE IF NOT EXISTS " + TABLE_ASSESSMENT + " ("
+            + ASSESSMENT_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + ASSESSMENT_NAME + " TEXT, "
+            + ASSESSMENT_DUE_DATE + " DATE, "
+            + ASSESSMENT_ASSESSMENT_TYPE_ID +  " INTEGER, "
+            + ASSESSMENT_COURSE_ID + " INTEGER,"
+            + "FOREIGN KEY ("+ASSESSMENT_TYPE_ID+") REFERENCES " + TABLE_ASSESSMENT_TYPE + "("+ASSESSMENT_TYPE_ID+") ON DELETE CASCADE,"
+            + "FOREIGN KEY ("+ASSESSMENT_COURSE_ID+") REFERENCES " + TABLE_COURSE + "("+COURSE_ID+") ON DELETE CASCADE)";
+
+    //Note Table
+    public static final String TABLE_NOTE = "note";
+    public static final String NOTE_ID = "noteId";
+    public static final String NOTE_CONTENTS = "noteContents";
+    public static final String NOTE_COURSE_ID = "noteCourseId";
+
+    //Note Table Create
+    private static final String CREATE_NOTE_TABLE =
+            "CREATE TABLE IF NOT EXISTS " + TABLE_NOTE + " ("
+            + NOTE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + NOTE_CONTENTS + " TEXT, "
+            + NOTE_COURSE_ID + " INTEGER,"
+            + "FOREIGN KEY ("+NOTE_COURSE_ID+") REFERENCES " + TABLE_COURSE + "("+COURSE_ID+") ON DELETE CASCADE)";
+
+    //Alert Table
+    public static final String TABLE_ALERT = "alert";
+    public static final String ALERT_ID = "alertId";
+    public static final String ALERT_NAME = "alertName";
+    public static final String ALERT_DATE_TIME = "alertDateTime";
+
+    //Alert Table Create
+    private static final String CREATE_ALERT_TABLE =
+            "CREATE TABLE IF NOT EXISTS " + TABLE_ALERT + " ("
+            + ALERT_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + ALERT_NAME + " TEXT, "
+            + ALERT_DATE_TIME + " DATETIME)";
+
+    public DbHandler(Context context){
+        super(context,DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        try {
+            db.execSQL(CREATE_TERM_TABLE);
+            db.execSQL(CREATE_COURSE_TABLE);
+            db.execSQL(CREATE_MENTOR_TABLE);
+            db.execSQL(CREATE_STATUS_TABLE);
+            db.execSQL(CREATE_ASSESSMENT_TABLE);
+            db.execSQL(CREATE_ASSESSMENT_TYPE_TABLE);
+            db.execSQL(CREATE_NOTE_TABLE);
+            db.execSQL(CREATE_ALERT_TABLE);
+        } catch (SQLException e) {
+            System.out.println("Error creating DB" + e);
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TERM);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_COURSE);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_MENTOR);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_STATUS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ASSESSMENT);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ASSESSMENT_TYPE);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NOTE);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ALERT);
+        onCreate(db);
+    }
+
+    public void createTestData(SQLiteDatabase db) {
+        try {
+            db.execSQL(GENERATE_TERM_TEST_DATA);
+            db.execSQL(GENERATE_MENTOR_TEST_DATA);
+            db.execSQL(GENERATE_COURSE_TEST_DATA);
+            db.execSQL(GENERATE_ASSESSMENT_TEST_DATA);
+        } catch (SQLException e) {
+            System.out.println("Error creating test data" + e);
+        }
     }
 
     //Term Table SelectAll
@@ -71,51 +208,6 @@ public class DbHandler extends SQLiteOpenHelper {
         return terms;
     }
 
-    //Term Table Select Term by Id
-    public Term selectTermById(int termId) {
-        SQLiteDatabase db = this.getReadableDatabase();
-
-        String selectQuery = "SELECT * FROM " + TABLE_TERM
-                + " WHERE " + TERM_ID + " = " + termId;
-
-        Log.e(LOG, selectQuery);
-
-        Cursor c = db.rawQuery(selectQuery, null);
-
-        if(c != null)
-            c.moveToFirst();
-
-        Term td = new Term();
-        td.setId(c.getInt((c.getColumnIndex(TERM_ID))));
-        td.setName(c.getString((c.getColumnIndex(TERM_NAME))));
-        td.setStartDate(c.getString((c.getColumnIndex(TERM_START_DATE))));
-
-        return td;
-    }
-
-    //Course Table
-    public static final String TABLE_COURSE = "course";
-    public static final String COURSE_ID = "courseId";
-    public static final String COURSE_NAME = "courseName";
-    public static final String COURSE_DESCRIPTION = "courseDescription";
-    public static final String COURSE_START_DATE = "courseStartDate";
-    public static final String COURSE_END_DATE = "courseEndDate";
-    public static final String COURSE_MENTOR_ID = "courseMentorId";
-    public static final String COURSE_STATUS_ID = "courseStatus";
-    public static final String COURSE_TERM_ID = "courseTermId";
-
-    //Course Table Create
-    private static final String CREATE_COURSE_TABLE =
-            "CREATE TABLE IF NOT EXISTS " + TABLE_COURSE + " ("
-            + COURSE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-            + COURSE_NAME + " TEXT, "
-            + COURSE_DESCRIPTION + " TEXT, "
-            + COURSE_START_DATE + " DATE, "
-            + COURSE_END_DATE + " DATE, "
-            + COURSE_MENTOR_ID + " INTEGER, "
-            + COURSE_STATUS_ID + " INTEGER, "
-            + COURSE_TERM_ID + " INTEGER)";
-
     //Select Courses by Term
     //TODO: Need to make Join on Mentor table and Term Table to get all data in same select so sets can use all of it.
 //    public List<Course> selectCoursesByTerm(int termId) {
@@ -140,35 +232,6 @@ public class DbHandler extends SQLiteOpenHelper {
 //        }
 //        return terms;
 //    }
-    //Select Course by Id
-
-
-
-    //Mentor Table
-    public static final String TABLE_MENTOR = "mentor";
-    public static final String MENTOR_ID = "mentorId";
-    public static final String MENTOR_NAME = "courseMentorName";
-    public static final String MENTOR_PHONE = "courseMentorPhone";
-    public static final String MENTOR_EMAIL = "courseMentorEmail";
-
-    //Mentor Table Create
-    private static final String CREATE_MENTOR_TABLE =
-            "CREATE TABLE IF NOT EXISTS " + TABLE_MENTOR + " ("
-            + MENTOR_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-            + MENTOR_NAME + " TEXT, "
-            + MENTOR_PHONE + " TEXT, "
-            + MENTOR_EMAIL + " TEXT)";
-
-    //Status Table
-    public static final String TABLE_STATUS = "status";
-    public static final String STATUS_ID = "statusId";
-    public static final String STATUS_NAME = "statusName";
-
-    //Status Table Create
-    private  static final String CREATE_STATUS_TABLE =
-            "CREATE TABLE IF NOT EXISTS " + TABLE_STATUS + " ("
-            + STATUS_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-            + STATUS_NAME + " TEXT)";
 
     //Status Table Setup
     private static final String INSERT_STATUS =
@@ -178,65 +241,11 @@ public class DbHandler extends SQLiteOpenHelper {
                     "('Completed')," +
                     "('Dropped')";
 
-    //Assessment Table
-    public static final String TABLE_ASSESSMENT = "assessment";
-    public static final String ASSESSMENT_ID = "assessmentId";
-    public static final String ASSESSMENT_NAME = "assessmentName";
-    public static final String ASSESSMENT_DUE_DATE = "assessmentDueDate";
-    public static final String ASSESSMENT_ASSESSMENT_TYPE_ID = "assessmentAssessmentTypeId";
-    public static final String ASSESSMENT_COURSE_ID = "assessmentCourseId";
-
-    //Assessment Table Create
-    private static final String CREATE_ASSESSMENT_TABLE =
-            "CREATE TABLE IF NOT EXISTS " + TABLE_ASSESSMENT + " ("
-            + ASSESSMENT_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-            + ASSESSMENT_NAME + " TEXT, "
-            + ASSESSMENT_DUE_DATE + " DATE, "
-            + ASSESSMENT_ASSESSMENT_TYPE_ID +  " INTEGER, "
-            + ASSESSMENT_COURSE_ID + " INTEGER)";
-
-    //Assessment Type Table
-    public static final String TABLE_ASSESSMENT_TYPE = "assessmentType";
-    public static final String ASSESSMENT_TYPE_ID = "assessmentTypeId";
-    public static final String ASSESSMENT_TYPE_NAME = "assessmentTypeName";
-
-    //Assessment Type Table Create
-    private static final String CREATE_ASSESSMENT_TYPE_TABLE =
-            "CREATE TABLE IF NOT EXISTS " + TABLE_ASSESSMENT_TYPE + " ("
-            + ASSESSMENT_TYPE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-            + ASSESSMENT_TYPE_NAME + " TEXT)";
-
-    ////Assessment Type Table Setup
+    //Assessment Type Table Setup
     private static final String INSERT_ASSESSMENT_TYPE =
             "INSERT INTO " + TABLE_ASSESSMENT_TYPE + " (" + ASSESSMENT_TYPE_NAME + ")" +
                     "VALUES ('Performance')," +
                     "('Objective')";
-
-    //Note Table
-    public static final String TABLE_NOTE = "note";
-    public static final String NOTE_ID = "noteId";
-    public static final String NOTE_CONTENTS = "noteContents";
-    public static final String NOTE_COURSE_ID = "noteCourseId";
-
-    //Note Table Create
-    private static final String CREATE_NOTE_TABLE =
-            "CREATE TABLE IF NOT EXISTS " + TABLE_NOTE + " ("
-            + NOTE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-            + NOTE_CONTENTS + " TEXT, "
-            + NOTE_COURSE_ID + " INTEGER)";
-
-    //Alert Table
-    public static final String TABLE_ALERT = "alert";
-    public static final String ALERT_ID = "alertId";
-    public static final String ALERT_NAME = "alertName";
-    public static final String ALERT_DATE_TIME = "alertDateTime";
-
-    //Alert Table Create
-    private static final String CREATE_ALERT_TABLE =
-            "CREATE TABLE IF NOT EXISTS " + TABLE_ALERT + " ("
-            + ALERT_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-            + ALERT_NAME + " TEXT, "
-            + ALERT_DATE_TIME + " DATETIME)";
 
     private static final String GENERATE_ASSESSMENT_TEST_DATA =
             "INSERT INTO " + TABLE_ASSESSMENT + " (" + ASSESSMENT_NAME + ", " + ASSESSMENT_DUE_DATE +
@@ -266,7 +275,7 @@ public class DbHandler extends SQLiteOpenHelper {
 
     private static final String GENERATE_MENTOR_TEST_DATA =
             "INSERT INTO " + TABLE_MENTOR + " (" + MENTOR_NAME + ", " + MENTOR_PHONE + ", " + MENTOR_EMAIL + ")\n" +
-            "VALUES ('James Smith', '123-456-1234', 'james.smith@wgu.edu'),\n" +
+                    "VALUES ('James Smith', '123-456-1234', 'james.smith@wgu.edu'),\n" +
                     "('Michael Smith', '123-456-1235', 'michael.smith@wgu.edu'),\n" +
                     "('Robert Smith', '123-456-1236', 'robert.smith@wgu.edu'),\n" +
                     "('Maria Garcia', '123-456-1237', 'maria.garcia@wgu.edu'),\n" +
@@ -278,53 +287,7 @@ public class DbHandler extends SQLiteOpenHelper {
 
     private static final String GENERATE_TERM_TEST_DATA =
             "INSERT INTO " + TABLE_TERM + " (" + TERM_NAME + ", " + TERM_START_DATE + ", " + TERM_END_DATE + ")\n" +
-            "VALUES ('Term 1', '2019-08-01', '2020-01-31'),\n" +
-            "('Term 2', '2020-02-01', '2020-07-31'),\n" +
-            "('Term 3', '2020-08-01', '2021-01-31')";
-
-    public DbHandler(Context context){
-        super(context,DB_NAME, null, DB_VERSION);
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        try {
-            db.execSQL(CREATE_TERM_TABLE);
-            db.execSQL(CREATE_COURSE_TABLE);
-            db.execSQL(CREATE_MENTOR_TABLE);
-            db.execSQL(CREATE_STATUS_TABLE);
-            db.execSQL(CREATE_ASSESSMENT_TABLE);
-            db.execSQL(CREATE_ASSESSMENT_TYPE_TABLE);
-            db.execSQL(CREATE_NOTE_TABLE);
-            db.execSQL(CREATE_ALERT_TABLE);
-            db.execSQL(INSERT_STATUS);
-            db.execSQL(INSERT_ASSESSMENT_TYPE);
-        } catch (SQLException e) {
-            System.out.println("Error creating DB" + e);
-        }
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TERM);
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_COURSE);
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_MENTOR);
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_STATUS);
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ASSESSMENT);
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ASSESSMENT_TYPE);
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NOTE);
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ALERT);
-        onCreate(db);
-    }
-
-    public void createTestData(SQLiteDatabase db) {
-        try {
-            db.execSQL(GENERATE_TERM_TEST_DATA);
-            db.execSQL(GENERATE_MENTOR_TEST_DATA);
-            db.execSQL(GENERATE_COURSE_TEST_DATA);
-            db.execSQL(GENERATE_ASSESSMENT_TEST_DATA);
-        } catch (SQLException e) {
-            System.out.println("Error creating test data" + e);
-        }
-    }
+                    "VALUES ('Term 1', '2019-08-01', '2020-01-31'),\n" +
+                    "('Term 2', '2020-02-01', '2020-07-31'),\n" +
+                    "('Term 3', '2020-08-01', '2021-01-31')";
 }
--- app/src/main/java/com/example/kylerdavisc196project/QueryManager.java
+++ app/src/main/java/com/example/kylerdavisc196project/QueryManager.java
@@ -2,6 +2,7 @@ package com.example.kylerdavisc196project;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
@@ -13,6 +14,9 @@ import com.example.kylerdavisc196project.model.Mentor;
 import com.example.kylerdavisc196project.model.Status;
 import com.example.kylerdavisc196project.model.Term;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class QueryManager {
     SQLiteOpenHelper dbHandler;
     SQLiteDatabase database;
@@ -21,10 +25,18 @@ public class QueryManager {
         dbHandler = new DbHandler(context);
     }
     public void open() {
-        Log.i("Read Query", "Database Opened");
+        Log.i("Write Query", "Database Opened");
         database = dbHandler.getWritableDatabase();
     }
+    //Term Operations
+    private static final String[] allTermColumns = {
+            DbHandler.TERM_ID,
+            DbHandler.TERM_NAME,
+            DbHandler.TERM_START_DATE,
+            DbHandler.TERM_END_DATE
 
+    };
+    //Create Term
     public long insertTerm(Term term) {
         ContentValues val = new ContentValues();
         val.put(DbHandler.TERM_NAME, term.getName());
@@ -33,7 +45,59 @@ public class QueryManager {
         long id = database.insert(DbHandler.TABLE_TERM,null, val);
         return id;
     }
+    //Read Term Operations
+    public Term selectTerm(int id) {
+        Cursor cursor = database.query(DbHandler.TABLE_TERM,allTermColumns,DbHandler.TERM_ID + "=?",new String[]{String.valueOf(id)},null,null, null, null);
+        if (cursor != null)
+            cursor.moveToFirst();
 
+        Term t = new Term(Integer.parseInt(cursor.getString(0)),cursor.getString(1),cursor.getString(2),cursor.getString(3));
+        // return Employee
+        return t;
+    }
+    public List<Term> selectAllTerms() {
+        Cursor cursor = database.query(DbHandler.TABLE_TERM,allTermColumns,null,null,null, null, null);
+        // Log.e(LOG, selectQuery);
+        List<Term> terms =  new ArrayList<>();
+
+        if (cursor.getCount()> 0) {
+            while(cursor.moveToNext()) {
+                Term td = new Term();
+                td.setId(cursor.getInt((cursor.getColumnIndex(DbHandler.TERM_ID))));
+                td.setName(cursor.getString((cursor.getColumnIndex(DbHandler.TERM_NAME))));
+                td.setStartDate(cursor.getString((cursor.getColumnIndex(DbHandler.TERM_START_DATE))));
+                terms.add(td);
+            }
+        }
+        return terms;
+    }
+    //Update Term
+    public long update(Term term) {
+        ContentValues val = new ContentValues();
+        val.put(DbHandler.TERM_NAME, term.getName());
+        val.put(DbHandler.TERM_START_DATE, term.getStartDate());
+        val.put(DbHandler.TERM_END_DATE, term.getEndDate());
+        long id = database.update(DbHandler.TABLE_TERM, val, DbHandler.TERM_ID + " = '" + term.getId() + "'", null);
+        return id;
+    }
+    //Delete Term
+    public long delete(Term term) {
+        long id = database.delete(DbHandler.TABLE_TERM, DbHandler.TERM_ID + " = " + term.getId(), null);
+        return id;
+    }
+
+    //Course Operations
+    public static final String[] allCourseColumns = {
+            DbHandler.COURSE_ID,
+            DbHandler.COURSE_NAME,
+            DbHandler.COURSE_DESCRIPTION,
+            DbHandler.COURSE_START_DATE,
+            DbHandler.COURSE_END_DATE,
+            DbHandler.COURSE_STATUS_ID,
+            DbHandler.COURSE_MENTOR_ID,
+            DbHandler.COURSE_TERM_ID
+    };
+    //Create Course
     public long insertCourse(Course course) {
         ContentValues val = new ContentValues();
         val.put(DbHandler.COURSE_NAME, course.getName());
@@ -46,6 +110,55 @@ public class QueryManager {
         long id = database.insert(DbHandler.TABLE_COURSE, null, val);
         return id;
     }
+    //Read Operations
+    public Term selectCourse(int id) {
+        Cursor cursor = database.query(DbHandler.TABLE_COURSE,allTermColumns,DbHandler.COURSE_ID + "=?",new String[]{String.valueOf(id)},null,null, null, null);
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        Course c = new Course(
+                Integer.parseInt(cursor.getString(0)),
+                        cursor.getString(1),
+                        cursor.getString(2),
+                        cursor.getString(3),
+                        cursor.getString(4),
+                        cursor.getInt(5),
+                        cursor.getInt(6),
+                        cursor.getInt(7));
+        // return Employee
+        return c;
+    }
+    public List<Term> selectAllTerms() {
+        Cursor cursor = database.query(DbHandler.TABLE_TERM,allTermColumns,null,null,null, null, null);
+        // Log.e(LOG, selectQuery);
+        List<Term> terms =  new ArrayList<>();
+
+        if (cursor.getCount()> 0) {
+            while(cursor.moveToNext()) {
+                Term td = new Term();
+                td.setId(cursor.getInt((cursor.getColumnIndex(DbHandler.TERM_ID))));
+                td.setName(cursor.getString((cursor.getColumnIndex(DbHandler.TERM_NAME))));
+                td.setStartDate(cursor.getString((cursor.getColumnIndex(DbHandler.TERM_START_DATE))));
+                terms.add(td);
+            }
+        }
+        return terms;
+    }
+
+    //Update
+    public long updateCourse(Course course) {
+        ContentValues val = new ContentValues();
+        val.put(DbHandler.COURSE_NAME, course.getName());
+        val.put(DbHandler.COURSE_DESCRIPTION, course.getDescription());
+        val.put(DbHandler.COURSE_START_DATE, course.getStartDate());
+        val.put(DbHandler.COURSE_END_DATE, course.getEndDate());
+        val.put(DbHandler.COURSE_STATUS_ID, course.getStatus().getId());
+        val.put(DbHandler.COURSE_MENTOR_ID, course.getMentor().getId());
+        val.put(DbHandler.COURSE_TERM_ID, course.getTerm().getId());
+        long id = database.update(DbHandler.TABLE_COURSE, val, DbHandler.COURSE_ID + "=" + course.getId(), null);
+        return id;
+    }
+
 
     public long insertMentor(Mentor mentor) {
         ContentValues val = new ContentValues();
